aa2ee778bc118ce7e04845e5d3ec1122a7465dcd
Leonardo Pernomian <leonardopernomian@hotmail.com>
2024-10-23 17:49:17 -0300

    mexendo um pouco mais em apis

---
+++ src/pages/api/form.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const usuarios: string[] = [];
+
+export default function form(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    post(req, res);
+  } else {
+    res.status(200).json(usuarios);
+  }
+  res.status(200).json({
+    dados: JSON.parse(req.body),
+  });
+}
+
+function post(req: NextApiRequest, res: NextApiResponse) {
+  const usuario = JSON.parse(req.body);
+  usuarios.push(usuario);
+  res.status(200).send;
+}
---
+++ src/pages/form.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+interface Usuario {
+  nome: string;
+  idade: string;
+}
+
+const form = () => {
+  const [nome, setNome] = useState<string>("");
+  const [idade, setIdade] = useState<number>(0);
+  const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+
+  const handleEnviar = async () => {
+    fetch("/api/form", {
+      method: "POST",
+      body: JSON.stringify({ nome, idade }),
+    });
+
+    const resp = await fetch("/api/form");
+    const usuarios = await resp.json();
+    setUsuarios(usuarios);
+  };
+
+  const renderizaUsuario = () => {
+    return usuarios.map((user, i) => {
+      return (
+        <li key={i}>
+          {user.nome} tem {user.idade}
+        </li>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h1>integrando com API</h1>
+      <input
+        type="text"
+        value={nome}
+        onChange={(e) => setNome(e.target.value)}
+      />
+      <input
+        type="number"
+        value={idade}
+        onChange={(e) => setIdade(+e.target.value)}
+      />
+      <button onClick={handleEnviar}>Enviar</button>
+      <ul>{renderizaUsuario()}</ul>
+    </div>
+  );
+};
+
+export default form;
